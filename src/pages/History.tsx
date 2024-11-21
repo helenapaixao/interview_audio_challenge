0639ea7afd4e16835ce2e11e97caadc943582247
helenapaixao <hp.helenapaixao@gmail.com>
2024-11-21 16:52:53 -0300

    chore: Update History component to improve audio playback and pagination

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -3,7 +3,7 @@
 import Header from "../components/Header";
 import { useEffect, useState } from "react";
 import { Toaster, toast } from "sonner";
-import { FaTrashAlt, FaFilter, FaChevronDown, FaHistory } from "react-icons/fa";
+import { FaTrashAlt, FaChevronDown, FaHistory } from "react-icons/fa";
 import { FaRegSun } from "react-icons/fa6";
 
 interface Recording {
@@ -110,7 +110,7 @@ const History = () => {
           <button
             onClick={() => setIsMenuOpen((prev) => !prev)}
             className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md flex items-center hover:bg-blue-600 transition"
-             type="button"
+            type="button"
           >
             <FaRegSun className="mr-2" />
             Configurações
@@ -147,7 +147,7 @@ const History = () => {
                     <button
                       onClick={resetFilters}
                       className="bg-gray-300 text-gray-700 px-4 py-2 rounded-md hover:bg-gray-400 transition"
-                       type="button"
+                      type="button"
                     >
                       Limpar Filtros
                     </button>
@@ -178,75 +178,95 @@ const History = () => {
           )}
         </div>
         <main className="mt-8">
-  {paginatedRecordings.length === 0 ? (
-    <div className="flex items-center justify-center h-96">
-      <p className="text-center text-gray-600">
-        Nenhuma gravação encontrada. Ajuste os filtros ou responda novas perguntas.
-      </p>
-    </div>
-  ) : (
-    <div className="overflow-x-auto shadow-lg rounded-lg border border-gray-200 mb-6">
-      <table className="w-full table-auto">
-        <thead className="bg-blue-600 text-white">
-          <tr>
-            <th className="px-6 py-4 text-left font-semibold">Pergunta</th>
-            <th className="px-6 py-4 text-left font-semibold">Áudio</th>
-            <th className="px-6 py-4 text-left font-semibold">Data e Horário</th>
-          </tr>
-        </thead>
-        <tbody>
-          {paginatedRecordings.map((recording, index) => (
-            <tr
-              key={index}
-              className={`${
-                index % 2 === 0 ? "bg-gray-50" : "bg-white"
-              } hover:bg-gray-100 transition duration-200`}
-            >
-              <td className="px-6 py-4 text-gray-800 font-medium break-words max-w-xs">
-                {recording.question}
-              </td>
-              <td className="px-6 py-4 flex justify-center items-center">
-                {recording.audioUrl === "Sem áudio gravado" ? (
-                  <span className="text-red-500 font-semibold">
-                    Áudio precisa ser gravado
-                  </span>
-                ) : (
-                  <audio
-                    src={recording.audioUrl}
-                    controls
-                    className="max-w-lg sm:max-w-md md:max-w-lg lg:max-w-xl h-14 rounded-lg shadow-sm border"
-                  >
-                    <track kind="captions" srcLang="en" label="English" />
-                    Seu navegador não suporta o elemento de áudio.
-                  </audio>
-                )}
-              </td>
-              <td className="px-6 py-4 text-gray-600 text-sm">
-                {recording.timestamp}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  )}
-  <div className="flex justify-center items-center space-x-4">
-    {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
-      <button
-        key={page}
-        onClick={() => setCurrentPage(page)}
-        className={`px-4 py-2 rounded-md ${
-          currentPage === page
-            ? "bg-blue-500 text-white"
-            : "bg-gray-300 text-gray-700 hover:bg-gray-400"
-        }`}
-      >
-        {page}
-      </button>
-    ))}
-  </div>
-</main>
-
+          {paginatedRecordings.length === 0 ? (
+            <div className="flex items-center justify-center h-96">
+              <p className="text-center text-gray-600">
+                Nenhuma gravação encontrada. Ajuste os filtros ou responda novas
+                perguntas.
+              </p>
+            </div>
+          ) : (
+            <div className="overflow-x-auto shadow-lg rounded-lg border border-gray-200 mb-6">
+              <table className="w-full table-auto">
+                <thead className="bg-blue-600 text-white">
+                  <tr>
+                    <th className="px-6 py-4 text-left font-semibold">
+                      Pergunta
+                    </th>
+                    <th className="px-6 py-4 text-left font-semibold">Áudio</th>
+                    <th className="px-6 py-4 text-left font-semibold">
+                      Data e Horário
+                    </th>
+                    <th className="px-6 py-4 text-left font-semibold">Ações</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {paginatedRecordings.map((recording, index) => (
+                    <tr
+                      key={index}
+                      className={`${
+                        index % 2 === 0 ? "bg-gray-50" : "bg-white"
+                      } hover:bg-gray-100 transition duration-200`}
+                    >
+                      <td className="px-6 py-4 text-gray-800 font-medium break-words max-w-xs">
+                        {recording.question}
+                      </td>
+                      <td className="px-6 py-4 flex justify-center items-center">
+                        {recording.audioUrl === "Sem áudio gravado" ? (
+                          <span className="text-red-500 font-semibold">
+                            Áudio precisa ser gravado
+                          </span>
+                        ) : (
+                          <audio
+                            src={recording.audioUrl}
+                            controls
+                            className="max-w-lg sm:max-w-md md:max-w-lg lg:max-w-xl h-14 rounded-lg shadow-sm border"
+                          >
+                            <track
+                              kind="captions"
+                              srcLang="en"
+                              label="English"
+                            />
+                            Seu navegador não suporta o elemento de áudio.
+                          </audio>
+                        )}
+                      </td>
+                      <td className="px-6 py-4 text-gray-600 text-sm">
+                        {recording.timestamp}
+                      </td>
+                      <td className="px-6 py-4">
+                        <button
+                          onClick={() => handleDeleteRecording(index)}
+                          className="px-3 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition flex items-center"
+                          type="button"
+                        >
+                          <FaTrashAlt className="mr-2" />
+                          Excluir
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+          <div className="flex justify-center items-center space-x-4">
+            {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
+              <button
+                key={page}
+                onClick={() => setCurrentPage(page)}
+                className={`px-4 py-2 rounded-md ${
+                  currentPage === page
+                    ? "bg-blue-500 text-white"
+                    : "bg-gray-300 text-gray-700 hover:bg-gray-400"
+                }`}
+                type="button"
+              >
+                {page}
+              </button>
+            ))}
+          </div>
+        </main>
       </div>
       <Toaster position="top-right" richColors />
     </>
